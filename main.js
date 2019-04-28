c713ca73f31336cad266f37224e51e0e827e7ef8
Jonas Davis <jonaspsv@gmail.com>
2019-04-28 15:54:32 +0200

    Picture slider
    Added a working picture slider (commented out for now) with main.js and changed the number to the correct one in contact.html

---
+++ main.js
@@ -0,0 +1,24 @@
+ var images = [
+  "images/Tr%C3%A6ningsfilosofi.jpg", 
+  "images/Kontakt.jpg", 
+  "images/Bonus.jpg"
+  ];
+  var num = 0;
+
+function next() {
+  var slider = document.getElementById("slider");
+  num++;
+  if(num >= images.length) {
+    num = 0;
+  }
+  slider.src = images[num];
+  }
+
+function prev() {
+  var slider = document.getElementById("slider");
+  num--;
+  if(num < 0) {
+    num = images.length-1;
+  }
+  slider.src = images[num];
+}
